3613047b235b65b9a8de4be0ae41d97ae048d96e
Mark Hartman <whispergreencorvus@gmail.com>
2022-04-01 15:58:30 +0200

    remove InputDataIsNullException class

--- src/main/java/com/hartmanmark/charcount/exception/InputDataIsNullException.java
+++
@@ -1,9 +0,0 @@
-package com.hartmanmark.charcount.exception;
-
-@SuppressWarnings("serial")
-public class InputDataIsNullException extends Exception{
-
-    public InputDataIsNullException(String message) {
-        super(message);
-    }
-}
